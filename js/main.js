6736a5ac453fac8bc254e393a6b1100c12a7641e
736500326-cyber <736500326@qq.com>
2026-07-23 18:13:48 +0800

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$("#book").turn({
-    width: 1200,
-    height: 1600,
-    autoCenter: true,
-    elevation:50,
-    gradients:true
-});
-$("#next").click(()=>$("#book").turn("next"));
-$("#prev").click(()=>$("#book").turn("previous"));
